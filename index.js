53848b443e3bbc5245013ea18324ac22151d9f5b
ejenrain15 <mambinunsad.earljohn@gmail.com>
2022-12-14 18:17:45 +0800

    Added API for contact

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ document.addEventListener("mousemove", (e) => {
 
 	body.insertAdjacentElement("beforeend", elem);
 })
+
+
+document.querySelector("#contact-form").addEventListener("submit", (e) =>{
+
+	e.preventDefault();
+
+	fetch("https://contact-api-3zlj.onrender.com/messages/send", {
+		method: "POST",
+		body: JSON.stringify({
+			fullName: document.querySelector("#full-name").value,
+			email: document.querySelector("#email").value,
+			message: document.querySelector("#message").value
+		}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	alert("Successfully Sent a Message")
+	document.querySelector("#full-name").value = null
+	document.querySelector("#email").value = null
+	document.querySelector("#message").value = null
+})
